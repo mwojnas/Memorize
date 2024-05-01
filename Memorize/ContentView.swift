60f0d519b6913131e3f6ce8c89e932d4d1f22291
mwojnas <matthias.woj@gmail.com>
2024-04-30 23:47:15 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Matt Wojnas on 2024-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Matt Wojnas on 2024-04-30.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
