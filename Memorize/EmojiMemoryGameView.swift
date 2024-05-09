4196798520476d30b2d45d1a1f9123d7c06d2d67
mwojnas <matthias.woj@gmail.com>
2024-05-09 18:17:27 -0400

    changes up to end of Lecture 5 includes adding game logic and basic animations

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -15,6 +15,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             Button("Suffle") {
                 viewModel.shuffle()
@@ -26,10 +27,13 @@ struct EmojiMemoryGameView: View {
     var cards: some View {
 
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
+            ForEach(viewModel.cards) { card in
+                    CardView(card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
             }
         }
         .foregroundColor(Color.orange)
@@ -59,6 +63,7 @@ struct CardView: View {
             base.fill()
                 .opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
