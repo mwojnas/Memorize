2eb10571b848baaa49f79378fd648a64fbb3e972
mwojnas <matthias.woj@gmail.com>
2024-05-26 16:33:25 -0400

    changes up to end of Lecture 7
    refactored CardView
    created constants for View
    created Pie shape, not yet animated

---
+++ Memorize/CardView.swift
@@ -0,0 +1,74 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Matt Wojnas on 2024-05-26.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+
+    let card: Card
+    
+    init(_ card: Card) {
+        self.card = card
+    }
+        
+    var body: some View {
+        Pie(endAngle: .degrees(240))
+            .opacity(Constants.Pie.opacity)
+            .overlay(
+                Text(card.content)
+                    .font(.system(size: Constants.FontSize.largest))
+                    .minimumScaleFactor(Constants.FontSize.scaleFactor)
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(Constants.Pie.inset)
+        )
+        .padding(Constants.inset)
+        .cardify(isFaceUp: card.isFaceUp)
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+    
+    private struct Constants {
+        static let inset: CGFloat = 5
+        struct FontSize {
+            static let largest: CGFloat = 200
+            static let smallest: CGFloat = 10
+            static let scaleFactor = smallest / largest
+        }
+        struct Pie {
+            static let opacity: CGFloat = 0.5
+            static let inset: CGFloat = 5
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    typealias Card = CardView.Card
+    
+    static var previews: some View {
+        VStack {
+            HStack {
+                CardView(Card(isFaceUp: true, content: "X", id: "test1"))
+                    .aspectRatio(4/3, contentMode: .fit)
+                CardView(Card(content: "X", id: "test1"))
+            }
+            HStack {
+                CardView(Card(isFaceUp: true, isMatched: true, content: "This is a very long string and I hope it fits", id: "test1"))
+                CardView(Card(isMatched: true, content: "X", id: "test1"))
+            }
+        }
+        .padding()
+        .foregroundColor(.green)
+    }
+}
+
+//#Preview {
+//    typealias Card = CardView.Card
+//    return CardView(Card(content: "X", id: "test1"))
+//        .padding()
+//        .foregroundColor(.green)
+//}
